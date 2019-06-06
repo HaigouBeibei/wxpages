2ec99580691eebfd91448d7b076747584bfb1c12
HaigouBeibei <haigoubeibei@163.com>
2019-06-06 22:17:55 +0800

    提交代码

--- Public/js/app.js
+++ Public/js/app.js
@@ -2,68 +2,72 @@ var appid = "wxf950c87150710871";
 var baseURL = "https://tongmeng.haigoubeibei.com/hiGou/";
 
 $.ajaxSetup({
-  cache: false,
-  xhrFields: {
-    withCredentials: true
-  },
-  complete: function(XMLHttpRequest, textStatus) {
-    var sessionstatus = XMLHttpRequest.getResponseHeader("sessionstatus");
-    if (sessionstatus == "TIMEOUT") {
-      var win = window;
-      while (win != win.top) {
-        win = win.top;
-      }
-      getCode();
+    cache: false,
+    xhrFields: {
+        withCredentials: true
+    },
+    complete: function(XMLHttpRequest, textStatus) {
+        var sessionstatus = XMLHttpRequest.getResponseHeader("sessionstatus");
+        if (sessionstatus == "TIMEOUT") {
+            var win = window;
+            while (win != win.top) {
+                win = win.top;
+            }
+            getCode();
+        }
     }
-  }
 });
 //获取CODE
 function getCode() {
-  //记忆地址以
-  //开头---6个/
-  //结尾---8个/
-  var jummpURL = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appid + "&redirect_uri=" + encodeURIComponent(baseURL + "wxpages/doAuth.html" + "?toURL=//////" + window.location.href + "////////") + "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
-  window.location.href = jummpURL;
+    //记忆地址以
+    //开头---6个/
+    //结尾---8个/
+    var jummpURL = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appid + "&redirect_uri=" + encodeURIComponent(baseURL + "wxpages/doAuth.html" + "?toURL=//////" + window.location.href + "////////") + "&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect";
+    window.location.href = jummpURL;
 }
 
 // 获取地址栏参数
 function getQueryString(name) {
-  var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-  var r = window.location.search.substr(1).match(reg);
-  if (r != null) return unescape(r[2]);
-  return null;
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
 }
 // 判断是否微信浏览器
 function isWeiXin() {
-  var ua = window.navigator.userAgent.toLowerCase();
-  if (ua.match(/MicroMessenger/i) == "micromessenger") {
-    return true;
-  } else {
-    return false;
-  }
+    var ua = window.navigator.userAgent.toLowerCase();
+    if (ua.match(/MicroMessenger/i) == "micromessenger") {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //显示错误信息
 function showError(showMessage) {
-  var errorForm = "";
-  errorForm += '<div class="am-modal am-modal-confirm" tabindex="-1" id="errorForm">';
-  errorForm += '<div class="am-modal-dialog">';
-  errorForm += '<div class="am-modal-hd">嗨购贝贝</div>';
-  errorForm += '<div class="am-modal-bd">';
-  errorForm += showMessage;
-  errorForm += "</div>";
-  errorForm += '<div class="am-modal-footer">';
-  errorForm += '<span class="am-modal-btn" data-am-modal-cancel>返回上一页</span>';
-  errorForm += '<span class="am-modal-btn" style="background-color:#f0250f;color:white" data-am-modal-confirm>刷新页面</span>';
-  errorForm += "</div></div></div>";
-  $("body").append(errorForm);
-  $("#errorForm").modal({
-    relatedTarget: this,
-    onConfirm: function(options) {
-      window.location.reload();
-    },
-    onCancel: function() {
-      history.back(-1);
-    }
-  });
+    var errorForm = "";
+    errorForm += '<div class="am-modal am-modal-confirm" tabindex="-1" id="errorForm">';
+    errorForm += '<div class="am-modal-dialog">';
+    errorForm += '<div class="am-modal-hd">嗨购贝贝</div>';
+    errorForm += '<div class="am-modal-bd">';
+    errorForm += showMessage;
+    errorForm += "</div>";
+    errorForm += '<div class="am-modal-footer">';
+    errorForm += '<span class="am-modal-btn" data-am-modal-cancel>返回上一页</span>';
+    errorForm += '<span class="am-modal-btn" style="background-color:#f0250f;color:white" data-am-modal-confirm>刷新页面</span>';
+    errorForm += "</div></div></div>";
+    $("body").append(errorForm);
+    $("#errorForm").modal({
+        relatedTarget: this,
+        onConfirm: function(options) {
+            window.location.reload();
+        },
+        onCancel: function() {
+            history.back(-1);
+        }
+    });
+}
+
+function handlePrice(price) {
+    return Math.floor(parseInt(price)) / 100;
 }
--- classify/js/goods_detail.js
+++ classify/js/goods_detail.js
@@ -1,5 +1,6 @@
 $(function() {
     initGoods();
+    initPlusReduce();
 });
 // 初始化Banner
 function initGoods() {
@@ -13,6 +14,7 @@ function initGoods() {
             showError("唔。。。服务器开了个小差，导致页面载入失败，放心，当你看到这个提示的时候，那个可怜的程序员小哥哥又被扣钱了，嘻嘻。");
         },
         success: function(data) {
+            console.log(data.d);
             if (data.c == 0) {
                 var datas = data.d;
                 //轮播图
@@ -47,12 +49,82 @@ function initSlide(data) {
 function initGoodsInfo(datas) {
     //title
     $("#goods_title").text(datas.name);
+    //price
+    $('.price').html('<strong>￥' + handlePrice(datas.minPrice) + ' <span class="am-icon-minus"></span>￥' + handlePrice(datas.maxPrice) + '</strong>');
     //content
     var content = "";
-    for (const key in datas.imgsDetailPageCotent) {
+    for (key in datas.imgsDetailPageCotent) {
         if (datas.imgsDetailPageCotent.hasOwnProperty(key)) {
             content += '<img src="' + baseURL + datas.imgsDetailPageCotent[key].path + '" class="am-img-responsive am-center" />';
         }
     }
     $("#content").empty().append(content);
+    //sku
+    $('#sku_goods_img').prop('src', baseURL + datas.imgListPage);
+    $('#sku_price').text('￥' + handlePrice(datas.minPrice));
+    $('#sku_stock').text('库存：请选择规格');
+    var skuContent = '';
+    for (key in datas.specs) {
+        if (datas.specs.hasOwnProperty(key)) {
+            skuContent += '<div class="am-g am-margin-top-xs">';
+            skuContent += '<p class="am-text-sm am-text-left am-padding-left-sm am-margin-bottom-xs"><strong>' + datas.specs[key].groupName + '</strong></p>';
+            skuContent += '</div>';
+            skuContent += '<div class="am-g">'
+            var specsValueData = datas.specs[key].values;
+            for (k in specsValueData) {
+                if (specsValueData.hasOwnProperty(k)) {
+                    skuContent += '<button type="button" class="am-btn am-btn-default am-round am-btn-xs">' + specsValueData[k].item + '</button>'
+                }
+            }
+            skuContent += '</div>';
+        }
+    }
+    $('#skuButtonsArea').empty().append(skuContent);
+    //sku Event
+    skuButtonsEvent();
+}
+
+function skuButtonsEvent() {
+    $('#skuButtonsArea .am-g button').click(function() {
+        $(this).addClass('selected').siblings().removeClass('selected');
+
+
+        // console.log($(this).parent('.am-g').find('button'));
+    });
+}
+
+function initPlusReduce() {
+    //加号按钮
+    $("#sku_count_plus").click(function() {
+        var countValue = parseInt($(this).prev().val());
+        var maxValue = parseInt($(this).prev().attr("max-value"));
+        var newValue = parseInt(countValue) + 1;
+        console.log(newValue);
+        var pop = $(this).prev().popover({
+            theme: "danger sm"
+        });
+        pop.popover("setContent", "已超过最大购买数");
+        if (newValue > maxValue) {
+            pop.popover("open");
+        } else {
+            pop.popover("close");
+            $(this).prev().val(newValue);
+        }
+    });
+    //减号按钮
+    $("#sku_count_reduce").click(function() {
+        var countValue = parseInt($(this).next().val());
+        var newValue = parseInt(countValue) - 1;
+        var pop = $(this).next().popover({
+            theme: "danger sm"
+        });
+        pop.popover("setContent", "至少购买一件商品");
+        console.log(newValue);
+        if (newValue < 1) {
+            pop.popover("open");
+        } else {
+            pop.popover("close");
+            $(this).next().val(newValue);
+        }
+    });
 }
