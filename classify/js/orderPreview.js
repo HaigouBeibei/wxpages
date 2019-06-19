4967ff1a99c5e965b4837a6fddbd4e2849a83942
HaigouBeibei <haigoubeibei@163.com>
2019-06-19 20:18:48 +0800

    提交代码

--- classify/js/orderPreview.js
+++ classify/js/orderPreview.js
@@ -1,7 +1,12 @@
+var skus = [];
 $(function() {
+    //初始化数据
     getOrderData();
+    //初始化提交订单按钮
+    initSubmitButton();
 });
 
+//初始化数据
 function getOrderData() {
     type = getQueryString('type');
     settlementInfo = {};
@@ -20,6 +25,7 @@ function getOrderData() {
     }
 }
 
+//获取数据
 function getData(settlementInfo) {
     $.ajax({
         url: baseURL + 'action/auth/user/normal/MerchantShopOrderAction/orderPreview',
@@ -42,6 +48,7 @@ function getData(settlementInfo) {
     });
 }
 
+//初始化收货地址
 function initPostMan(data) {
     postHref = window.location.href.substring(0, window.location.href.indexOf('deliveryAddressId') - 1);
     $('#postMan').text('收货人：' + data.addressInfo.recipients + ' ' + data.addressInfo.contactNumber);
@@ -49,6 +56,7 @@ function initPostMan(data) {
     $('#postHref').attr('href', baseURL + 'wxpages/i/wh-myAddress.html?callback=' + '//////' + postHref + '////////');
 }
 
+//向页面写入数据
 function writeData(data) {
     appendContent = '';
     if (data.listValid.length > 0) {
@@ -68,17 +76,57 @@ function writeData(data) {
                             appendContent += '<p class="am-margin-0 am-text-right">运费 ：￥' + data.listValid[key].skus[x].freight + '</p>';
                             appendContent += '<p class="price am-text-right am-margin-top-0 am-margin-bottom-sm">￥' + handlePrice(data.listValid[key].skus[x].price) + ' <span class="am-icon-remove"></span> ' + data.listValid[key].skus[x].count + '</p>';
                             appendContent += '</div>';
+                            tempObj = {
+                                "id": data.listValid[key].skus[x].id,
+                                "count": data.listValid[key].skus[x].count
+                            }
+                            skus.push(JSON.stringify(tempObj));
                         }
                     }
                 }
                 appendContent += '<div class="am-cf"></div>';
                 appendContent += '<div class="am-u-sm-12 divsion"></div>';
-                appendContent += '<p class="am-text-right am-margin-xs am-margin-bottom-0">共' + data.listValid[key].subtotalCount + '件商品，小计：<span class="am-text-danger">￥' + handlePrice(data.listValid[key].subtotalPrice) + '</span></p>';
+                appendContent += '<p class="am-text-right am-margin-xs am-margin-bottom-0">共' + data.listValid[key].subtotalCount + '件商品<br />商品总价：<span class="am-text-danger">￥' + handlePrice(data.listValid[key].subtotalGoodsAmount) + '</span><br />运费：<span class="am-text-danger">￥' + handlePrice(data.listValid[key].subtotalFreight) + '</span><br />小计：<span class="am-text-danger">￥' + handlePrice(data.listValid[key].subtotalAmount) + '</span></p>';
                 appendContent += '</div>';
             }
         }
     }
+    skus = '[' + skus.toString() + ']';
     $('#goodsList').empty();
     $('#goodsList').append(appendContent);
     $('#finalCount').text('￥' + handlePrice(data.amount));
+}
+
+//初始化提交订单按钮
+function initSubmitButton() {
+    $('#submitButton').click(function() {
+        console.log(skus);
+        console.log(getQueryString('deliveryAddressId'));
+        $.ajax({
+            url: baseURL + 'action/auth/user/normal/MerchantShopOrderAction/orderGenerate',
+            data: {
+                deliveryAddressId: getQueryString('deliveryAddressId'),
+                skus: skus
+            },
+            success: function(data) {
+                datas = data.d;
+                ids = [];
+                if (data.c == 0) {
+                    for (const key in datas) {
+                        if (datas.hasOwnProperty(key)) {
+                            ids.push(datas[key]);
+                        }
+                    }
+                    ids = ids.join(',');
+                    //console.log(baseURL + "wxpages/pay/index.html?ids=" + ids);
+                    window.location.href = baseURL + "wxpages/pay/index.html?ids=" + ids;
+                }
+            },
+            error: function(data) {
+                console.log(data);
+                showError(data.m);
+            }
+        });
+
+    });
 }
--- pay/js/index.js
+++ pay/js/index.js
@@ -1,9 +1,12 @@
+var payment;
 $(function() {
     //初始化默认方式
-    var payment = $('input[data-am-ucheck]').eq(0).val();
+    payment = $('input[data-am-ucheck]').eq(0).val();
     initCheck();
     //确认支付按钮事件
-    initConfirmPayButtonEvent();
+    $("#confirmPayButton").click(function() {
+        initConfirmPayButtonEvent();
+    })
 });
 
 //支付方式选择框
@@ -12,21 +15,53 @@ function initCheck() {
         $('input[data-am-ucheck]').uCheck('uncheck');
         $(this).uCheck('check');
         payment = $(this).val();
-        console.log(payment);
     });
 }
+// alert(JSON.toString(data))
+
+var paymentSuccessCallback = function(data) {};
+var paymentFailCallback = function(data) {};
+var paymentCancelCallback = function(data) {};
 
 //确认支付按钮事件
 function initConfirmPayButtonEvent() {
     $(this).click(function() {
         $.ajax({
-            url: baseURL + '',
+            url: baseURL + 'action/auth/user/normal/MerchantShopOrderAction/unifiedOrder',
             data: {
-                payment: payment,
+                channel: 'wechatJsApi',
+                ids: getQueryString('ids')
             },
             success: function(data) {
                 if (data.c == 0) {
-                    //...
+                    wx.chooseWXPay({
+                        // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                        timestamp: data.d.timeStamp,
+                        // 支付签名随机串，不长于 32 位
+                        nonceStr: data.d.nonceStr,
+                        // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
+                        package: data.d.package,
+                        // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+                        signType: data.d.signType,
+                        // 支付签名
+                        paySign: data.d.paySign,
+                        success: function(res) {
+                            if (res.errMsg == "chooseWXPay:ok") {
+                                // 使用以上方式判断前端返回,微信团队郑重提示：res.errMsg将在用户支付成功后返回
+                                // chooseWXPay:ok，但并不保证它绝对可靠。
+                                paymentSuccessCallback(data);
+                            } else {
+                                //目前不知道什么情况代码会运行到这，但为防万一就写它了
+                                paymentFailCallback(data);
+                            }
+                        },
+                        cancel: function(res) {
+                            paymentCancelCallback(data);
+                        },
+                        fail: function(res) {
+                            paymentFailCallback(data);
+                        }
+                    });
                 }
             },
             error: function(data) {
